0c91765bf26f457e2a8b477a1ecdfc3a23fdf74b
tiod <tiod@issetapc>
2014-07-03 23:38:26 +0300

    ewewew

--- gHelper/src/helper/Gh.java
+++ gHelper/src/helper/Gh.java
@@ -12,48 +12,47 @@ import java.net.UnknownHostException;
 import java.security.CodeSource;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.Random;
 import java.util.Set;
 
 public class Gh {
-
-	public static SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
-	public static SimpleDateFormat ft1 = new SimpleDateFormat("H:mm:ss.SSS");
-	public static SimpleDateFormat ft2 = new SimpleDateFormat("yyyy-MM-dd H:mm:ss.SSS");
-	public SimpleDateFormat ft3 = new SimpleDateFormat("H:mm:ss");
-	public SimpleDateFormat ft4 = new SimpleDateFormat("yyyy-MM-dd H:mm:ss");
-	private static SimpleDateFormat ft5 = new SimpleDateFormat("yyyy-MM");
-	public DecimalFormat df2 = new DecimalFormat("#.##");
-
-	public boolean shuttingDown = false;
-
+	
+	public static SimpleDateFormat	ft				= new SimpleDateFormat("yyyy-MM-dd");
+	public static SimpleDateFormat	ft1				= new SimpleDateFormat("H:mm:ss.SSS");
+	public static SimpleDateFormat	ft2				= new SimpleDateFormat("yyyy-MM-dd H:mm:ss.SSS");
+	public SimpleDateFormat			ft3				= new SimpleDateFormat("H:mm:ss");
+	public SimpleDateFormat			ft4				= new SimpleDateFormat("yyyy-MM-dd H:mm:ss");
+	private static SimpleDateFormat	ft5				= new SimpleDateFormat("yyyy-MM");
+	public DecimalFormat			df2				= new DecimalFormat("#.##");
+	
+	public boolean					shuttingDown	= false;
+	
 	public Gh() {
-
+		
 	}
-
+	
 	public String t() {
 		return ft1.format(System.currentTimeMillis());
 	}
-
+	
 	public String tt() {
 		return ft3.format(System.currentTimeMillis());
 	}
-
+	
 	public static String td() {
 		return ft.format(System.currentTimeMillis());
 	}
-
+	
 	public static String td2() {
 		return ft5.format(System.currentTimeMillis());
 	}
-
+	
 	public static String getJarFolder(CodeSource codeSource) {
-
+		
 		File jarFile = null;
-
+		
 		try {
 			jarFile = new File(codeSource.getLocation().toURI().getPath());
-
+			
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
@@ -61,7 +60,7 @@ public class Gh {
 		jarDir = jarDir.replace("\\", "/");
 		return jarDir;
 	}
-
+	
 	public String gDecoder(String text, Byte position) {
 		// prnt("decoder received: " + text);
 		String[] tokens = null;
@@ -70,7 +69,7 @@ public class Gh {
 		} catch (Exception e) {
 			Glog.prnte("gDecoder, error, text=" + text + ", e=" + e.getMessage());
 		}
-
+		
 		if (tokens != null) {
 			// prnt("decoded: " + tokens[position]);
 			return tokens[position];
@@ -78,14 +77,14 @@ public class Gh {
 			return "error, cannot decode";
 		}
 	}
-
+	
 	public String fileDateModified(String fullFileName) {
 		File file = new File(fullFileName);
 		Glog.prnt("fileDateModified: fullFileName=" + fullFileName);
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return sdf.format(file.lastModified());
 	}
-
+	
 	public static String getRunningFileName() {
 		String path = Gh.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 		String decodedPath = null;
@@ -97,32 +96,32 @@ public class Gh {
 		// System.out.println("decodedPath=" + decodedPath);
 		File f = new File(decodedPath);
 		// System.out.println(f.getName());
-
+		
 		return f.getName();
 	}
-
+	
 	public static String getPcAndUserName() {
 		String userName = "error cannot get user name";
-
+		
 		try {
 			userName = System.getProperty("user.name");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		
 		String pcName = "error cannot get pc name";
-
+		
 		try {
 			pcName = InetAddress.getLocalHost().getHostName();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-
+		
 		pcName = pcName + "#" + userName;
-
+		
 		return pcName;
 	}
-
+	
 	public Integer getInteger(String text) {
 		try {
 			return Integer.valueOf(text);
@@ -131,7 +130,7 @@ public class Gh {
 			return null;
 		}
 	}
-
+	
 	public BigDecimal getBigDecimal(String text) {
 		try {
 			return new BigDecimal(text);
@@ -140,62 +139,62 @@ public class Gh {
 			return null;
 		}
 	}
-
+	
 	public Boolean getBoolean(Object object, String key) {
-
+		
 		Boolean booleanValue = null;
-
+		
 		try {
 			booleanValue = Boolean.valueOf((String) object);
 		} catch (Exception e) {
 			Glog.prnte("LoadSett2: getBoolean, problem setting: " + key.toString() + "=" + object.toString() + e.getMessage());
 		}
-
+		
 		return booleanValue;
 	}
-
+	
 	public String getString(Object object, String key) {
-
+		
 		String string = null;
-
+		
 		try {
 			string = String.valueOf((String) object);
 		} catch (Exception e) {
 			Glog.prnte("LoadSett2: getString, problem setting: " + key.toString() + "=" + object.toString() + " e=" + e.getMessage());
 		}
-
+		
 		return string;
 	}
-
+	
 	public Integer getInteger(Object object, String key) {
-
+		
 		Integer intNumber = null;
-
+		
 		try {
 			intNumber = Integer.valueOf((String) object);
 		} catch (Exception e) {
 			Glog.prnte("LoadSett2: getInteger, problem setting: " + key.toString() + "=" + object.toString() + " e=" + e.getMessage());
 		}
-
+		
 		return intNumber;
 	}
-
+	
 	public Float getFloat(Object object, String key) {
-
+		
 		Float floatNumber = null;
-
+		
 		try {
 			floatNumber = Float.valueOf((String) object);
-
+			
 		} catch (Exception e) {
 			Glog.prnte("LoadSett2: getFloat, problem setting: " + key.toString() + "=" + object.toString() + " e=" + e.getMessage());
 		}
-
+		
 		return floatNumber;
 	}
-
+	
 	public static void runSystemCommand(String command) {
-
+		
 		try {
 			Runtime.getRuntime().exec(command);
 //			BufferedReader inputStream = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -205,14 +204,14 @@ public class Gh {
 //			while ((s = inputStream.readLine()) != null) {
 //				prnt("[ping]" + s);
 //			}
-
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public Window getSelectedWindow(Window[] windows) {
-
+		
 		Window result = null;
 		for (int i = 0; i < windows.length; i++) {
 			Window window = windows[i];
@@ -227,16 +226,16 @@ public class Gh {
 		}
 		return result;
 	}
-
+	
 	public double roundDouble(double value, int places) {
 		if (places < 0)
 			throw new IllegalArgumentException();
-
+		
 		BigDecimal bd = new BigDecimal(value);
 		bd = bd.setScale(places, RoundingMode.HALF_UP);
 		return bd.doubleValue();
 	}
-
+	
 	public String gRound(Double notRoundedNbr, Integer digits) {
 		if (notRoundedNbr != null) {
 			String roundedNbr = new BigDecimal(notRoundedNbr).setScale(digits, RoundingMode.HALF_UP).toString();
@@ -245,7 +244,7 @@ public class Gh {
 			return null;
 		}
 	}
-
+	
 	public String gRound(Float notRoundedNbr, Integer digits) {
 		if (notRoundedNbr != null) {
 			String roundedNbr = new BigDecimal(notRoundedNbr).setScale(digits, RoundingMode.HALF_UP).toString();
@@ -254,7 +253,7 @@ public class Gh {
 			return null;
 		}
 	}
-
+	
 	public String gRound(BigDecimal notRoundedNbr, Integer digits) {
 		if (notRoundedNbr != null) {
 			String roundedNbr = notRoundedNbr.setScale(digits, RoundingMode.HALF_UP).toString();
@@ -263,23 +262,23 @@ public class Gh {
 			return null;
 		}
 	}
-
+	
 	public static Thread getThreadByName(String threadName) {
 		Thread __tmp = null;
-
+		
 		Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
 		Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
-
+		
 		for (int i = 0; i < threadArray.length; i++) {
 			if (threadArray[i].getName().equals(threadName))
 				__tmp = threadArray[i];
 		}
-
+		
 		return __tmp;
 	}
-
-	private static final int POW10[] = { 1, 10, 100, 1000, 10000, 100000, 1000000 };
-
+	
+	private static final int	POW10[]	= { 1, 10, 100, 1000, 10000, 100000, 1000000 };
+	
 	public String g2Round(double val, int precision) {
 		StringBuilder sb = new StringBuilder();
 		if (val < 0) {
@@ -296,11 +295,11 @@ public class Gh {
 		sb.append(fval);
 		return sb.toString();
 	}
-
+	
 	public String decodeFast(String fieldName, String text) {
 		int index1 = 0, index2, index3;
 		String res1, res2, res3;
-
+		
 //		int count = ;
 //		int count2 = ;
 //		if ((text.length() - text.replace("=", "").length()) != (text.length() - text.replace(";", "").length())) {
@@ -320,22 +319,22 @@ public class Gh {
 			System.out.println("index1=" + index1);
 			return "[error]";
 		}
-
+		
 //		System.out.println("res1= " + res1);
 //		System.out.println("res2= " + res2);
 //		System.out.println("res3= " + res3);
-
+		
 //		String res2 = text.substring(text.indexOf(fieldName)).substring(0, text.indexOf(";"));
 //		int index3 = res2.indexOf("=");
-
+		
 //		return res2.substring(index3 + 1);
-
+		
 	}
-
+	
 	public String getUniq() {
-		Random random = new Random();
-		int randomNumber = random.nextInt(90000 - 10000) + 10000;
-		return String.valueOf(randomNumber) + String.valueOf(System.currentTimeMillis());
-		//return String.valueOf((int) (Math.random() * (900000 - 100000)) + 100000) + String.valueOf(System.currentTimeMillis());
+		//Random random = new Random();
+		//int randomNumber = random.nextInt(9000 - 1000) + 1000;
+		//return System.currentTimeMillis();
+		return String.valueOf((int) (Math.random() * (900000 - 100000)) + 100000) + String.valueOf(System.currentTimeMillis());
 	}
 }
--- gHelper/src/mysql/Mysql.java
+++ gHelper/src/mysql/Mysql.java
@@ -1,5 +1,6 @@
 package mysql;
 
+import helper.Gh;
 import helper.Glog;
 
 import java.sql.Connection;
@@ -10,43 +11,47 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Mysql {
-
+	
 	public static void main(String[] args) {
 		Glog.runPrintLogToConsole();
+		Gh gh = new Gh();
 		Glog.prnt("start");
 		Mysql dao = new Mysql();
 		dao.connectDb();
 		//dao.updateActorName(7);
 		//System.out.println("actor name=" + dao.requestActorName(7));
-
-		Runnable r1 = new Runnable() {
-			public void run() {
-				for (int i = 0; i < 100; i++) {
-					dao.insertInto();
-				}
-				Glog.prnt("end inserting");
-			}
-		};
-
-		Thread thr1 = new Thread(r1);
-		thr1.start();
-		Glog.prnt("end");
-
+		
+//		Runnable r1 = new Runnable() {
+//			public void run() {
+//				for (int i = 0; i < 100; i++) {
+//					dao.inertNewTrade();
+//				}
+//				Glog.prnt("end inserting");
+//			}
+//		};
+//		
+//		Thread thr1 = new Thread(r1);
+//		thr1.start();
+//		Glog.prnt("end");
+		
+		dao.inertNewTrade(gh.getUniq());
+		dao.updateTradeField("2871821404408764774", "tradeSide", "buy");
+		
 	}
-
-	private Connection conn = null;
-	private Statement st = null;
-	private PreparedStatement preparedStatement = null;
-	private ResultSet resultSet = null;
-
+	
+	private Connection			conn				= null;
+	private Statement			st					= null;
+	private PreparedStatement	preparedStatement	= null;
+	private ResultSet			resultSet			= null;
+	
 	public void connectDb() {
-
+		
 		String url = "jdbc:mysql://localhost:3306/";
 		String dbName = "sakila";
 		String driver = "com.mysql.jdbc.Driver";
 		String userName = "user1";
-		String password = "qaz123";
-
+		String password = "test123";
+		
 		try {
 			Class.forName(driver).newInstance();
 		} catch (InstantiationException e) {
@@ -56,33 +61,32 @@ public class Mysql {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-
+		
 		try {
 			conn = DriverManager.getConnection(url + dbName, userName, password);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
+		
 		Glog.prnt(conn.toString());
-
+		
 		try {
 			st = conn.createStatement();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
+		
 	}
-
+	
 	public String requestActorName(Integer actorId) {
-
 		try {
 			resultSet = st.executeQuery("SELECT first_name FROM sakila.actor WHERE actor_id = " + actorId);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
+		
 		String msg = null;
-
+		
 		try {
 			while (resultSet.next()) {
 				//int id = resultSet.getInt("actor_id");
@@ -91,25 +95,26 @@ public class Mysql {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
+		
 		return msg;
-
 	}
-
-	public void updateActorName(Integer actorId) {
+	
+	public void updateTradeField(String tradeId, String fieldName, String fieldValue) {
 		try {
-			st.executeUpdate("UPDATE sakila.actor SET first_name='janis7' WHERE actor_id = " + actorId + ";");
+			st.executeUpdate("UPDATE skudra.trades SET " + fieldName + "='" + fieldValue + "', lastTableUpdate=NOW() WHERE tradeId = " + tradeId
+					+ ";");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-
-	public void insertInto() {
+	
+	public void inertNewTrade(String tradeId) {
+		Glog.prnt("nbr = " + tradeId);
 		try {
-			st.execute("INSERT INTO sakila.actor(first_name, last_name) VALUES ('namaaae', 'ddddd')");
+			//st.execute("INSERT INTO skudra.trades(tradeId, tradeSide, requestedPrice, filledPrice) VALUES (4, 'buy', 1.12345, 1.12344)");
+			st.execute("INSERT INTO skudra.trades(tradeId, dateTime) VALUES ( " + tradeId + ", NOW())");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
