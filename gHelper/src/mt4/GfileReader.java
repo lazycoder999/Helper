5bbba46a5c562bc0cd28da1018a20fc81b5b15e6
hmm <hmm2>
2014-09-29 18:05:05 +0300

    ewewew

--- gHelper/src/mt4/GfileReader.java
+++ gHelper/src/mt4/GfileReader.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class GfileReader {
-	
+
 //	public static void main(String[] args) {
 //		//"C://Users//kepe//AppData//Roaming//MetaQuotes//Terminal//299541A8EEF200DE23B8B72470C7D8FA//MQL4//Files//pipe.txt";
 //		//"C://Users//tiod//AppData//Roaming//MetaQuotes//Terminal//CCD68BFB06049A8615C607C3F6AD69B7//MQL4//Files//pipe.txt";
@@ -17,13 +17,13 @@ public class GfileReader {
 //		String precise = "//MQL4//Files//poga.txt";
 //		nm.StartClient(location + precise, 25, (byte) 1);
 //	}
-	
-	private ReaderListener	readerListener;
-	
+
+	private ReaderListener readerListener;
+
 	public void setReadrListener(ReaderListener readerListener) {
 		this.readerListener = readerListener;
 	}
-	
+
 	public void StartClient(String fileName, int refreshRate, byte listenerId, boolean sendOnChange, byte lineNumb) {
 		//Glog.runPrintLogToConsole();
 //		Glog.prnt("started file reader. fileName=" + fileName + ", refreshRate=" + refreshRate + ", listenerId=" + listenerId + ", sendOnChange="
@@ -31,16 +31,16 @@ public class GfileReader {
 		String prev_line = "aaa";
 		RandomAccessFile pipe = null;
 		while (true) {
-			
+
 			File myTestFile = new File(fileName);
 			boolean fileRead = myTestFile.canRead();
 			boolean fileExists = myTestFile.exists();
 			boolean fileIs = myTestFile.isFile();
-			
+
 			if (fileRead && fileExists && fileIs) {
-				
+
 				pipe = null;
-				
+
 				try {
 					pipe = new RandomAccessFile(fileName, "r");
 				} catch (FileNotFoundException e) {
@@ -53,7 +53,7 @@ public class GfileReader {
 					Glog.prnte("manual checks error 3");
 					e.printStackTrace();
 				}
-				
+
 				if (pipe != null) {
 					String line = null;
 					byte lineNumbCnt = 0;
@@ -62,11 +62,17 @@ public class GfileReader {
 							if ((!prev_line.equals(line) || !sendOnChange) && lineNumbCnt == lineNumb) {
 								//Glog.prnt("readed line=" + line);
 								prev_line = line;
-								
+
 								if (listenerId == 1) {
 									readerListener.incomingFileMsg1(prev_line);
 								} else if (listenerId == 2) {
 									readerListener.incomingFileMsg2(prev_line);
+								} else if (listenerId == 3) {
+									readerListener.incomingFileMsg3(prev_line);
+								} else if (listenerId == 4) {
+									readerListener.incomingFileMsg4(prev_line);
+								} else if (listenerId == 5) {
+									readerListener.incomingFileMsg5(prev_line);
 								}
 							}
 							lineNumbCnt++;
@@ -90,6 +96,6 @@ public class GfileReader {
 				Glog.prnte("fileName=" + fileName + " listenerId=" + listenerId);
 			}
 		}
-		
+
 	}
 }
